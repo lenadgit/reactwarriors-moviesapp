cae6c8dff18fbca4ce28a2333b801b20e4e5297d
Lena <lena.boruslav@gmail.com>
2020-03-19 23:44:57 +0200

    moviesapp

---
+++ reactwarriors-moviesapp/precache-manifest.e96e5817e31147bb014f1a3da01c11d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e16d7d869d17ee5cbda4459fd345ee",
+    "url": "/reactwarriors-movieapp/index.html"
+  },
+  {
+    "revision": "f8842e037463bea04415",
+    "url": "/reactwarriors-movieapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a95516810df283d38304",
+    "url": "/reactwarriors-movieapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8842e037463bea04415",
+    "url": "/reactwarriors-movieapp/static/js/2.1d4bbde3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactwarriors-movieapp/static/js/2.1d4bbde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95516810df283d38304",
+    "url": "/reactwarriors-movieapp/static/js/main.9262cf06.chunk.js"
+  },
+  {
+    "revision": "1643bee243c672c167f1",
+    "url": "/reactwarriors-movieapp/static/js/runtime-main.6ef72283.js"
+  }
+]);
